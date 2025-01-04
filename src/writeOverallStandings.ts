c8974188ce149fc246f543e4ab3882846055e588
Adam Faulkner <adzfaulkner@hotmail.com>
2025-01-04 21:30:51 +0000

    write overall standings - 3

--- src/writeOverallStandings.ts
+++ src/writeOverallStandings.ts
@@ -1,13 +1,36 @@
 import { ranges, PREVIOUS_SCHEDULE_IDS } from './config'
+import * as console from "node:console";
+
+const aliases = new Map([
+    ['London Eagles', 'London Eagles Green']
+])
 
 export const writeOverallStandings = (): void => {
     //const ss: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.getActive()
+
+    const perf: Map<string, number> = new Map()
 
     PREVIOUS_SCHEDULE_IDS.forEach(id => {
         const s = SpreadsheetApp.openById(id)
         const standings = s.getSheetByName('Final Standings')
             .getRange(ranges.finalStanding)
 
-        console.log(standings.getValues())
+        standings.getValues().forEach(v => {
+            let [,team,pts] = v
+
+            if (aliases.has(team)) {
+                team = aliases.get(team)
+            }
+
+            if (!perf.has(team)) {
+                perf.set(team, 0)
+            }
+
+            perf.set(team, perf.get(team) + parseInt(pts, 10))
+        })
     })
+
+    const overall = Array.from(perf)
+
+    console.log(overall)
 }
