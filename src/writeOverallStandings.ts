fc00cc140b29fb2cb76a7ade4a320333cdcf5ebe
Adam Faulkner <adzfaulkner@hotmail.com>
2025-01-04 21:00:31 +0000

    write overall standings - 2

--- src/writeOverallStandings.ts
+++ src/writeOverallStandings.ts
@@ -1,5 +1,4 @@
 import { ranges, PREVIOUS_SCHEDULE_IDS } from './config'
-import * as console from "node:console";
 
 export const writeOverallStandings = (): void => {
     //const ss: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.getActive()
